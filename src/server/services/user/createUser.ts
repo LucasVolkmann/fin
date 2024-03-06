8184444d622103f5183c3f8e9597b296954b8753
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-03-06 18:20:21 -0300

    feat: insert hashed passwords

--- src/server/services/user/createUser.ts
+++ src/server/services/user/createUser.ts
@@ -2,6 +2,7 @@ import { User } from '../../models/User';
 import { UserDTOType } from '../../models/dtos/UserDTOType';
 import { EmailAlreadyExistsError } from '../../shared/exceptions/EmailAlreadyExistsError';
 import { InternalServerError } from '../../shared/exceptions/InternalServerError';
+import { PasswordCrypto } from '../../shared/functions/hash';
 import { getByEmailUser } from './getByEmailUser';
 
 export const createUser = async (userDTO: UserDTOType): Promise<number | void> => {
@@ -11,7 +12,9 @@ export const createUser = async (userDTO: UserDTOType): Promise<number | void> =
     throw new EmailAlreadyExistsError('This e-mail already exists.');
   }
 
-  const user = new User(userDTO.username, userDTO.email, userDTO.password);
+  const hashedPassword = await PasswordCrypto.hashPassword(userDTO.password);
+
+  const user = new User(userDTO.username, userDTO.email, hashedPassword);
 
   const newUser = await user.save();
   if(!newUser.id) {
---
+++ src/server/shared/functions/hash.ts
@@ -0,0 +1,18 @@
+import { compare, genSalt, hash } from 'bcryptjs';
+
+const SALT_ROUNDS = 4;
+
+const hashPassword = async (password: string) => {
+  const salt = await genSalt(SALT_ROUNDS);
+
+  return await hash(password, salt);
+};
+
+const verifyPassword = async (password: string, hashedPassword:string) => {
+  return await compare(password, hashedPassword);
+};
+
+export const PasswordCrypto = {
+  hashPassword,
+  verifyPassword
+};
